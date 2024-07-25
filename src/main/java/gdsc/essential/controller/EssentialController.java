f8bac8b2bf8960239992177c436c62a35352c447
rhkrqkq <rhkrqkq1025@gmail.com>
2024-07-25 14:24:47 +0900

    assignment2 edit

--- src/main/java/gdsc/essential/controller/EssentialController.java
+++ src/main/java/gdsc/essential/controller/EssentialController.java
@@ -1,5 +1,47 @@
 package gdsc.essential.controller;
 
+import gdsc.essential.Speaker;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+@RestController
 public class EssentialController {
 
+    private Speaker targetSpeaker;
+
+    @PostMapping("/submit")
+    public ResponseEntity<String> enrollSpeaker(@RequestBody Speaker sp) {
+        targetSpeaker = new Speaker(
+                sp.getDate(),
+                sp.getTopic(),
+                sp.getContent()
+        );
+        return ResponseEntity.ok("등록 완료");
+    }
+
+    @GetMapping("/submit/date")
+    public ResponseEntity<String> submitDate(@RequestParam String id) {
+
+        if (targetSpeaker != null && targetSpeaker.equals(id)) {
+            String date = targetSpeaker.getDate();
+            if (date != null && date.isEmpty()) {
+                return ResponseEntity.ok("발표 날짜는 " + date + " 입니다.");
+            }
+        }
+        return ResponseEntity.badRequest().body("해당 발표자는 없습니다.");
+    }
+
+    @GetMapping("/submit/info")
+    public ResponseEntity<?> getSpeaker() {
+
+        if (targetSpeaker == null) {
+            return ResponseEntity.badRequest().body("발표자가 없습니다.");
+        }
+
+        return ResponseEntity.ok(targetSpeaker);
+    }
 }
